7e1483b7953c4e81e5f321f81fbef0f406e89513
Guilherme <guilherme@codebit.com.br>
2023-09-14 23:05:49 -0300

    tasks

--- src/pages/Task/List/index.js
+++ src/pages/Task/List/index.js
@@ -8,24 +8,54 @@ import { useParams, useLocation } from 'react-router-dom';
 import * as S from './style';
 import EditDelete from '../../../components/Form/EditDelete';
 import ListContentKanban from '../../../components/ListContentKanban';
+import { formattedDate } from '../../../GeneralFunctions/functions';
+import FilterContent from '../../../components/FilterContent';
 
 function TaskKanban() {
   const [data, setData] = useState([]);
-  const [load, setLoad] = useState(false);
+  const [columnsExcel, setColumnsExcel] = useState(false);
   const { id } = useParams();
   const url = `/tasks`
+  const urlUserTasks = `/user-tasks`
   const location = useLocation();
+  const [user, setUser] = useState(true);
+  useEffect(() => {
+    setUser(JSON.parse(localStorage.getItem('user')))
+  }, []);
 
-  async function loadData() {
-    await get(url)
+  async function loadData(clean) {
+    let userAux = JSON.parse(localStorage.getItem('user'))
+    if(userAux.Role.status ==4){
+
+      await get(url)
+      .then(async response => {
+        let list = [];
+        if (response) {
+          response.records.map(item =>{
+            item.details = item.description
+            let obj = {
+              id: item.id,
+              Time: item.name,
+              Criação: formattedDate(item.createdAt),
+            }
+            list.push(obj)
+          })
+          setData(response.records);
+          setColumnsExcel(list);
+        }
+      });
+    }else{
+      await get(`${urlUserTasks}/${userAux.id}`)
       .then(async response => {
         if (response) {
-         response.records.map(item =>{
+          response.records.map(item =>{
             item.details = item.description
           })
+          console.log(response)
           setData(response.records);
         }
       });
+    }
 
   }
 
@@ -33,14 +63,16 @@ function TaskKanban() {
     loadData();
   }, [])
 
+  function cleanFilter(){
+    loadData(true);
+  }
+
   return (
     <Container>
       <HeaderContent title="Tarefas" icon={<Task fontSize="large" />} titleButton="Nova Tarefa" linkTo="/tasks/novo" />
-      {/* <FilterContent columnsExcel={columnsExcel} filesheet={"Tarefas"} fileName={"tasks.xlsx"} loadData={() => loadData()} cleanFilter={() => cleanFilter()}> */}
-        {/* <InputFormFilter options={supplierOptions} selected={supplierSelected} setSelected={setSupplierSelected} value={supplierSelected} setValue={setSupplierSelected} title="Fornecedor" type='select' size="small"></InputFormFilter>
-        <InputFormFilter options={statusOptions} selected={statusSelected} setSelected={setStatusSelected} value={statusSelected} setValue={setStatusSelected} title="Status" type='select' size="small"></InputFormFilter> */}
-        {/* <InputFormFilter value={endDate} setValue={setEndDate} title="Prazo" type='date' size="small"></InputFormFilter> */}
-      {/* </FilterContent> */}
+      <FilterContent columnsExcel={columnsExcel} filesheet={"Tarefas"} fileName={"tasks.xlsx"} loadData={() => loadData()} cleanFilter={() => cleanFilter()}>
+       {/* <InputFormFilter options={supplierOptions} selected={supplierSelected} setSelected={setSupplierSelected} value={supplierSelected} setValue={setSupplierSelected} title="Fornecedor" type='select' size="small"></InputFormFilter>  */}
+      </FilterContent>
       <ListContentKanban data={data} setData={setData}/>
 
     </Container>
